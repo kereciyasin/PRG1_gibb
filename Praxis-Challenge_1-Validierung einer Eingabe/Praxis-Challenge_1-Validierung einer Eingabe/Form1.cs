9387ea1feb919267a86cc2f51372a5cb61eb003c
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-22 14:37:09 +0200

    Projekt erstellt

---
+++ Praxis-Challenge_1-Validierung einer Eingabe/Praxis-Challenge_1-Validierung einer Eingabe/Form1.cs
@@ -0,0 +1,10 @@
+namespace Praxis_Challenge_1_Validierung_einer_Eingabe
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Praxis-Challenge_1-Validierung einer Eingabe/Praxis-Challenge_1-Validierung einer Eingabe/Program.cs
@@ -0,0 +1,17 @@
+namespace Praxis_Challenge_1_Validierung_einer_Eingabe
+{
+	internal static class Program
+	{
+		/// <summary>
+		///  The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			// To customize application configuration such as set high DPI settings or default font,
+			// see https://aka.ms/applicationconfiguration.
+			ApplicationConfiguration.Initialize();
+			Application.Run(new Form1());
+		}
+	}
+}
